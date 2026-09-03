653f586c2dbb0a046275d222f492c144f3f26392
Jadson-Borges <jadsonborgesdecastro@hotmail.com>
2026-09-03 17:51:17 -0300

    feat: adiciona entidades de dominio Usuario, TipoRota, StatusRota, Rota, DepesaMensal

---
+++ src/SistemaCombustivel.Domain/Entities/DespesaMensal.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SistemaCombustivel.Domain.Entities
+{
+    internal class DespesaMensal
+    {
+        
+    }
+}
